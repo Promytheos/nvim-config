39f2150cb7fa5b309f83728c2720d193d2ae7be4
luke.swan <luke.swan@gamesys.ee>
2024-03-25 17:54:10 +0200

    fixing group prefix

--- lua/config/gitsigns-config.lua
+++ lua/config/gitsigns-config.lua
@@ -11,7 +11,7 @@ return {
     current_line_blame_opts = {},
     on_attach = function (bufnr)
         local gs = package.loaded.gitsigns
-        local gitGroup = require("utils.mappings").get_group_prefix('<leader>', 'l')
+        local gitGroup = require("utils.mappings").get_group_prefix('<leader>', 'g')
 
         gitGroup['b'] = { gs.toggle_current_line_blame, 'Toggle Current Line Blame' }
         gitGroup['B'] = { function ()
