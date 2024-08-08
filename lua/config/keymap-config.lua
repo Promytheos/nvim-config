1494da1da4572307edf5af0cdc34bc8d33150960
Luke Swan <swan.luke9@gmail.com>
2024-08-09 01:55:50 +0300

    Update icons

--- lua/config/keymap-config.lua
+++ lua/config/keymap-config.lua
@@ -1,71 +1,85 @@
-require("toggleterm").setup {}
-local terminal = require("toggleterm.terminal").Terminal
-local lazygit = terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
-function Lazygit_Toggle()
-  lazygit:toggle()
-end
+local gs = package.loaded.gitsigns
 
 return {
-  { "-",                "<cmd>Oil<cr>",                   desc = "Open Parent Dir in Oil" },
-  { "<leader><leader>", "<cmd>Alpha<cr>",                 desc = "Open Dashboard" },
+  { "-", "<cmd>Oil<cr>", desc = "Open Parent Dir in Oil" },
+  { "<leader><leader>", "<cmd>Alpha<cr>", desc = "Dashboard", icon = '' },
 
-  { "<leader>f",        group = "file" }, -- group
-  { "<leader>ff",       "<cmd>Telescope find_files<cr>",  desc = "Find File",             mode = "n" },
-  { "<leader>fg",       "<cmd>Telescope grep_string<cr>", desc = "Find String",           mode = "n" },
-  { "<leader>fh",       "<cmd>Telescope help_tags<cr>",   desc = "Help Tags",             mode = "n" },
-  { "<leader>fh",       "<cmd>Telescope oldfiles<cr>",    desc = "Recent Files",          mode = "n" },
-  { "<leader>f?",       "<cmd>Telescope builtin<cr>",     desc = "All Commands",          mode = "n" },
+  -- Telescope Functions
+  { "<leader>f", group = "Telescope" },
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+  { "<leader>fg", "<cmd>Telescope grep_string<cr>", desc = "Find String", mode = "n" },
+  { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags", mode = "n" },
+  { "<leader>fh", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files", mode = "n" },
+  { "<leader>f?", "<cmd>Telescope builtin<cr>", desc = "All Commands", mode = "n" },
 
+  -- Buffer Functions
   {
     "<leader>b",
-    group = "buffers",
+    group = "Buffers",
     expand = function()
       return require("which-key.extras").expand.buf()
     end
   },
-  { "<A-x>",      "<cmd>:lua require(\"mini.bufremove\").delete(0)<cr>", desc = "Delete Current Buffer" },
-  { "<A-X>",      "<cmd>1,.-bd | .+1,$bd<cr>",                           desc = "Delete Other Buffers" },
-  { "<A-h>",      "<cmd>bp<cr>",                                         desc = "Go to Previous Buffer" },
-  { "<A-l>",      "<cmd>bn<cr>",                                         desc = "Go to Next Buffer" },
-  { "<A-o>",      "<cmd>b#<cr>",                                         desc = "Toggle Last Active Buffer" },
-  { "<A-t>",      "<cmd>tabnew<cr>",                                     desc = "Create New Tab" },
-  { "<A-w>",      "<cmd>tabc<cr>",                                       desc = "Close Tab" },
+  { "<A-x>", "<cmd>:lua require(\"mini.bufremove\").delete(0)<cr>", desc = "Delete Current Buffer" },
+  { "<A-X>", "<cmd>1,.-bd | .+1,$bd<cr>", desc = "Delete Other Buffers" },
+  { "<A-h>", "<cmd>bp<cr>", desc = "Go to Previous Buffer" },
+  { "<A-l>", "<cmd>bn<cr>", desc = "Go to Next Buffer" },
+  { "<A-o>", "<cmd>b#<cr>", desc = "Toggle Last Active Buffer" },
+  { "<A-t>", "<cmd>tabnew<cr>", desc = "Create New Tab" },
+  { "<A-w>", "<cmd>tabc<cr>", desc = "Close Tab" },
 
-  { "<leader>p",  group = "plugins" },
-  { "<leader>pm", "<cmd>Mason<cr>",                                      desc = "Open Mason LSP Plugin Manager" },
-  { "<leader>pl", "<cmd>Lazy<cr>",                                       desc = "Open Lazy Plugin Manager" },
+  -- Plugin Functions
+  { "<leader>p", group = "Plugins", icon = "" },
+  { "<leader>pm", "<cmd>Mason<cr>", desc = "Open Mason LSP Plugin Manager" },
+  { "<leader>pl", "<cmd>Lazy<cr>", desc = "Open Lazy Plugin Manager" },
 
-  { "<leader>t",  group = "terminal" },
-  { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>",                 desc = "ToggleTerm float" },
-  { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>",            desc = "ToggleTerm horizontal" },
-  { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>",              desc = "ToggleTerm vertical" },
-  { "<leader>tl", "<cmd>lua Lazygit_Toggle()<cr>",                       desc = "ToggleTerm lazygit" },
-  { "<F7>",       "<cmd>ToggleTermToggleAll<cr>",                        desc = "Toggle Terminal",              mode = "nt" },
+  -- ToggleTerm Functions
+  { "<leader>t", group = "Terminal" },
+  { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float" },
+  { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizontal" },
+  { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical" },
+  {
+    "<leader>tl",
+    function()
+      local terminal = require("toggleterm.terminal").Terminal
+      local lazygit = terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+      lazygit:toggle()
+    end,
+    desc = "Lazygit"
+  },
+  { "<F7>",       "<cmd>ToggleTermToggleAll<cr>", desc = "Toggle Terminal",          mode = "nt" },
 
+  { "<leader>g",  group = "Git" },
+  { "<leader>gb", gs.toggle_current_line_blame,   desc = 'Toggle Current Line Blame' },
+  {
+    "<leader>gB",
+    function()
+      gs.blame_line { full = true }
+    end,
+    desc = 'Toggle Full Line Blame'
+  },
 
   -- Vim Built-In Functions
-  { "<Esc>",      "<cmd>nohlsearch<cr>",                                 desc = "Clear Highlights" },
-  { "<C-s>",      "<cmd>w<cr>",                                          desc = "Write" },
-  { "<C-S-s>",    "<cmd>wa<cr>",                                         desc = "Write All" },
-  { "<C-q>q",     "<cmd>confirm qall<cr>",                               desc = "Confirm Quit All" },
-  { "<C-q>w",     "<cmd>confirm q<cr>",                                  desc = "Confirm Quit Window" },
-  { "<C-q>b",     "<cmd>confirm bd<cr>",                                 desc = "Confirm Quit Buffer" },
-  { "<C-q>f",     "<cmd>qa!<cr>",                                        desc = "Force Quit" },
+  { "<Esc>",   "<cmd>nohlsearch<cr>",       desc = "Clear Highlights" },
+  { "<C-s>",   "<cmd>w<cr>",                desc = "Write" },
+  { "<C-S-s>", "<cmd>wa<cr>",               desc = "Write All" },
+  { "<C-q>q",  "<cmd>confirm qall<cr>",     desc = "Confirm Quit All" },
+  { "<C-q>w",  "<cmd>confirm q<cr>",        desc = "Confirm Quit Window" },
+  { "<C-q>b",  "<cmd>confirm bd<cr>",       desc = "Confirm Quit Buffer" },
+  { "<C-q>f",  "<cmd>qa!<cr>",              desc = "Force Quit" },
 
-  { "|",          "<cmd>vsplit<cr>",                                     desc = "Vertical Split" },
-  { "\\",         "<cmd>split<cr>",                                      desc = "Horizontal Split" },
+  { "|",       "<cmd>vsplit<cr>",           desc = "Vertical Split" },
+  { "\\",      "<cmd>split<cr>",            desc = "Horizontal Split" },
 
-  { "<C-A-k>",    ":t .-1<cr>",                                          desc = "Copy Line Up" },
-  { "<C-A-k>",    ":'<,'>t '<-1<cr>gv",                                  desc = "Copy Lines Up",                mode = 'v' },
-  { "<C-A-j>",    ":t .<cr>",                                            desc = "Copy Line Down" },
-  { "<C-A-j>",    ":'<,'>t '><cr>gv",                                    desc = "Copy Lines Down",              mode = 'v' },
+  { "<C-A-k>", ":t .-1<cr>",                desc = "Copy Line Up" },
+  { "<C-A-k>", ":'<,'>t '<-1<cr>gv",        desc = "Copy Lines Up",      mode = 'v' },
+  { "<C-A-j>", ":t .<cr>",                  desc = "Copy Line Down" },
+  { "<C-A-j>", ":'<,'>t '><cr>gv",          desc = "Copy Lines Down",    mode = 'v' },
 
-  { "<C-S-k>",    ":m .-2<cr>",                                          desc = "Move Line Up" },
-  { "<C-S-k>",    ":'<,'>m '<-2<cr>gv",                                  desc = "Move Lines Up",                mode = 'v' },
-  { "<C-S-j>",    ":m .+1<cr>",                                          desc = "Move Line Down" },
-  { "<C-S-j>",    ":'<,'>m '>+1<cr>gv",                                  desc = "Move Lines Down",              mode = 'v' },
+  { "<C-S-k>", ":m .-2<cr>",                desc = "Move Line Up" },
+  { "<C-S-k>", ":'<,'>m '<-2<cr>gv",        desc = "Move Lines Up",      mode = 'v' },
+  { "<C-S-j>", ":m .+1<cr>",                desc = "Move Line Down" },
+  { "<C-S-j>", ":'<,'>m '>+1<cr>gv",        desc = "Move Lines Down",    mode = 'v' },
 
-  { "<C-i>",      "<cmd>Precognition peek<cr>" },
-
-
+  { "<C-i>",   "<cmd>Precognition peek<cr>" },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,77 +1,79 @@
 return {
-    {
-        'goolord/alpha-nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-            'nvim-lua/plenary.nvim'
-        },
-        config = function()
-            require("alpha").setup(require("alpha.themes.startify").config)
-        end
+  {
+    'goolord/alpha-nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim'
     },
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require("gitsigns").setup(require("config.gitsigns-config"))
-        end
-    },
-    {
-        'stevearc/dressing.nvim', opts = {},
-    },
-    {
-        'b0o/incline.nvim',
-        config = function()
-            require('incline').setup()
-        end,
-        -- Optional: Lazy load Incline
-        event = 'VeryLazy',
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/noice.nvim' },
-        config = function()
-            local lualine_require = require("lualine_require")
-            lualine_require.require = require
+    config = function()
+      require("alpha").setup(require("alpha.themes.startify").config)
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require("gitsigns").setup(require("config.gitsigns-config"))
+    end
+  },
+  {
+    'stevearc/dressing.nvim', opts = {},
+  },
+  {
+    'b0o/incline.nvim',
+    config = function()
+      require('incline').setup()
+    end,
+    -- Optional: Lazy load Incline
+    event = 'VeryLazy',
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/noice.nvim' },
+    config = function()
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
 
-            vim.o.laststatus = vim.g.lualine_laststatus
-            require('lualine').setup(require("config.lualine-config"))
-        end
-    },
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup(require("config.catppuccin-config"))
-        end
-    },
-    {
-        'folke/which-key.nvim',
-        event = 'VeryLazy',
-        opts = {}
-    },
-    {
-        'folke/todo-comments.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim'
-        },
-        opts = {
-            signs = false
-        }
-    },
-    {
-        'folke/trouble.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-        opts = {},
-    },
-    {
-        'tris203/precognition.nvim',
-        event = 'VeryLazy',
-        opts = {
-            startVisible = false,
-        },
+      vim.o.laststatus = vim.g.lualine_laststatus
+      require('lualine').setup(require("config.lualine-config"))
+    end
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup(require("config.catppuccin-config"))
+    end
+  },
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      preset = "helix",
     }
+  },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    opts = {
+      signs = false
+    }
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
+    opts = {},
+  },
+  {
+    'tris203/precognition.nvim',
+    event = 'VeryLazy',
+    opts = {
+      startVisible = false,
+    },
+  }
 }
