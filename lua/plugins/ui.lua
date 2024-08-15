3582c9d83acc3f0cb3efd9dfd172ae5962af847b
luke.swan <luke.swan@gamesys.ee>
2024-08-15 15:55:09 +0300

    Update trouble config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -59,7 +59,7 @@ return {
       'nvim-lua/plenary.nvim'
     },
     opts = {
-      signs = false
+      signs = true
     }
   },
   {
@@ -68,6 +68,19 @@ return {
       'nvim-tree/nvim-web-devicons'
     },
     opts = {},
+  },
+  {
+    'folke/flash.nvim',
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
   },
   {
     'tris203/precognition.nvim',
